d23bbbaf4e322bb30aa8caac9b14ef0d4db6a603
Rohan Singh <singhro6@msu.edu>
2019-11-12 21:23:21 +0000

    Project 2

--- Project2.cpp
+++ Project2.cpp
@@ -14,7 +14,8 @@ using std::accumulate;
 #include<stdio.h> 
 #include<string.h> 
 
-int comp_func(vector<char> vec1, vector<char> vec2){
+//custom compare funtion that returns how close two passwords are to each other
+int comp_func(const vector<char> &vec1,const vector<char> &vec2){
   if (vec1.size() < vec2.size()){
     std::swap(vec1,vec2);
   }
@@ -28,18 +29,22 @@ int comp_func(vector<char> vec1, vector<char> vec2){
 }
 
 int main(){
+    
+  //Input password
   string password;
   cout << "Give me a password:" << endl;
   cin >> password;
   cout << "You provided a password of " << password << endl;
   std::ifstream myfile;
 
-	myfile.open("common_passwords.txt");
+  //imput file of passwords to compare
+  myfile.open("common_passwords.txt");
   vector<string> similar;
   vector<string> similar2;
   vector<char> pass_char(password.begin(), password.end());
+  
+  // finds closest number
   string fstring;
-//hey nice code mannnnnnnnn
   int min = 10;
 	while (true){
 	   if (myfile >> fstring){
@@ -54,18 +59,17 @@ int main(){
 	   	break;
 	   }
   }
+  
+  //uses closest number to find all passwords that match
   for(int i = 0; i < static_cast<int>(similar.size()); i++){
     vector<char> compare(similar[i].begin(), similar[i].end());
     int limit = comp_func(pass_char, compare);
     if(limit == min){
       similar2.push_back(similar[i]);
     }
-	}
-
-
-  //int change = compare(pass_char, compare);
-
-
+  }
+  
+  //outputs most similar passwords
   cout << "The most similar passwords to " << password << " are:" << endl;
   std::sort(similar2.begin(), similar2.end());
   for(int j = 0; j < static_cast<int>(similar2.size()); j++) {
